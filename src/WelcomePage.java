492bdb59518cec7fd75bf946233930fe8f793a41
Zulhilmim633 <muhammad.zulhilmizaid@gmail.com>
2022-06-18 22:01:08 +0800

    modified

--- src/WelcomePage.java
+++ src/WelcomePage.java
@@ -15,6 +15,7 @@ public class WelcomePage extends JFrame implements ActionListener {
 	 */
 	public static void main(String[] args) {
         WelcomePage frame = new WelcomePage();
+        frame.setVisible(true);
 	}
 
 	/**
@@ -59,18 +60,21 @@ public class WelcomePage extends JFrame implements ActionListener {
 		setBounds(100, 100, 450, 300);
         setSize(329,268);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
 	}
 
     public void actionPerformed(ActionEvent e){
         if(e.getSource() == nextBtn){
-            if(group.getSelection().getActionCommand().equalsIgnoreCase("compete")){
+            if(group.getSelection() == null){
+                JOptionPane.showMessageDialog(null,"Need selection","Error", JOptionPane.ERROR_MESSAGE);
+            }else if(group.getSelection().getActionCommand().equalsIgnoreCase("compete")){
                 Compete compete = new Compete();
+                compete.setVisible(true);
                 dispose();
             }else if(group.getSelection().getActionCommand().equalsIgnoreCase("visitor")){
                 Visitor visitor = new Visitor();
+                visitor.setVisible(true);
                 dispose();
-            }
+            }                
         }
     }
 }
